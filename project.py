80038fb378b36a134400d3647aebb69e237314e9
Mochni <arsenijlandysev@gmail.com>
2026-04-21 17:44:31 +0300

    Update project.py

--- project.py
+++ project.py
@@ -15,8 +15,8 @@ def get_ai_recommendation(item_type, genre, author, character, length, mood, ext
     
     models_to_try = [
         "nvidia/nemotron-3-super-120b-a12b:free",
-        "google/gemini-2.0-pro-exp-02-05:free",
-        "mistralai/mistral-7b-instruct:free",
+        "openai/gpt-oss-120b:free",
+        "nvidia/nemotron-3-nano-30b-a3b:free",
         "deepseek/deepseek-chat:free"
     ]
     
